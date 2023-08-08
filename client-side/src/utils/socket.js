a9c5f7fe07213670a12e651f60d7fbb692b29aaa
mohaned <mohanedmohamed41@gmail.com>
2023-08-08 15:02:47 +0300

    "update env"

--- client-side/src/utils/socket.js
+++ client-side/src/utils/socket.js
@@ -1,3 +1,3 @@
 import io from "socket.io-client"; // to connect with socket.io in client side => npm i socket.io-client
 
-export const socket = io.connect(process.env.REACT_SERVER);
+export const socket = io.connect("http://chat-app-bay-nu.vercel.app");
